d7c841252138a2f6b1aa1ffd5c663aa494a747de
Mike <ezuratechan@gmail.com>
2017-07-24 18:04:26 -0400

    cleaned up some code and added error checking in parameters

--- b
+++ b
@@ -13,10 +13,15 @@ buildpage() {
     for var in "$@"
     do
         ./a.out "$SITE" $var $STYLEGAJSHEADER $KEYWORDS > $OUTDIR/$var.$OUTEXT 
-        pandoc $PAGESDIR/$var.$INEXT -t html >> $OUTDIR/$var.$OUTEXT 
-        cat $FOOTERFILE >> $OUTDIR/$var.$OUTEXT 
-        echo "Page $var built"
-    done
+        #echo $?
+        if [ $? -eq 0 ]; then
+            pandoc $PAGESDIR/$var.$INEXT -t html >> $OUTDIR/$var.$OUTEXT 
+            cat $FOOTERFILE >> $OUTDIR/$var.$OUTEXT 
+            echo "Page $var built"
+        else
+            echo "Error in last command. Exiting"
+        fi
+        done
 }
 
 buildpage $@
--- main.c
+++ main.c
@@ -1,61 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-
+#include <unistd.h>
+#define BUFFER_SIZE 1024
 int main(int argc, char *argv[]) {
-    assert(argc==5);
+    if (argc!=5) {
+        perror("Argument count is wrong");
+        exit(-1);
+    }
     int argv1_notempty = strcmp(argv[1], "")!=0;
     int argv2_notempty = strcmp(argv[2], "")!=0;
     int argv3_notempty = strcmp(argv[3], "")!=0;
     int argv4_notempty = strcmp(argv[4], "")!=0;
-    int case0 = argv1_notempty && argv2_notempty && argv3_notempty && argv4_notempty;
-    int case1 = argv1_notempty && (!argv2_notempty) && argv3_notempty && argv4_notempty;
-    assert( case0 || case1 );
-
+    int checkfornotempty134 = argv1_notempty && argv3_notempty && argv4_notempty;
+    if (!checkfornotempty134) {
+        perror("One or more of your arguments are empty");
+        exit(-1);
+    }
     printf("<!DOCTYPE html><html><head>");
-    if (strcmp(argv[2], "")==0) { // ./a.out "sitename" "" "style_gajs_header.txt" "keywords.txt"
+    if (!argv2_notempty) { // ./a.out "sitename" "" "style_gajs_header.txt" "keywords.txt"
         printf("<title>%s</title>\n", argv[1]);
     }
     else { // ./a.out "sitename" "pagename" "style_gajs_header.txt" "keywords.txt"
         printf("<title>%s | %s</title>\n", argv[1], argv[2]);
     }
-
-    char *STYLE_GAJS_HEADER_FILENAME = argv[3];
-    char *KEYWORDS_FILENAME = argv[4];
-    printf("<meta charset=\"UTF-8\"/>\n");
-    printf("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>\n");   
-    printf("<meta name=\"keywords\" content=\"");
-
-    // read in and print out keywords
-    FILE *infile = fopen(KEYWORDS_FILENAME, "r");
-    char buffer[1024]={0};
+    int styleFilenameExists = access(argv[3], R_OK) != -1;
+    int keywordsFilenameExists = access(argv[4], R_OK) != -1;
+    if (! (styleFilenameExists && keywordsFilenameExists)) {
+        perror("An error has occurred");
+        exit(-1);
+    }
+    printf("<meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/><meta name=\"keywords\" content=\"");
+    FILE *infile = fopen(argv[4], "r");// read in and print out keywords
+    char buffer[BUFFER_SIZE]={0};
     while (!feof(infile)) {
-    	fgets(buffer, 1024, infile);
-        int i = 0;
-        for (i = strlen(buffer)-1; i >= 0; i--) { 
+    	fgets(buffer, BUFFER_SIZE, infile);
+        for (int i = strlen(buffer)-1; i >= 0; i--) { 
             if (buffer[i] == '\n') {
                 buffer[i] = '\0';
                 break;
             }
         }
         printf("%s, ", buffer);
-        // zero out the buffer
-        for (int i = 0; i < 1024; i++) {
-            buffer[i] = 0;
-        }
+        memset(buffer, 0, BUFFER_SIZE);
     }
     fclose(infile);
-    printf("\b\b\b\b");
-    printf("\"/>\n");
-
-    // cat out the aggregate style + analytics header file
-    infile = fopen(STYLE_GAJS_HEADER_FILENAME, "r");
-    for (int i = 0; i < 1024; i++) {
-        buffer[i] = 0;
-    }
+    printf("\b\b\b\b\"/>\n");
+    infile = fopen(argv[3], "r");// cat out the aggregate style + analytics header file
+    memset(buffer, 0, BUFFER_SIZE);
     while (!feof(infile)) {
-    	fgets(buffer, 1024, infile);
+    	fgets(buffer, BUFFER_SIZE, infile);
     	printf("%s", buffer);
     }
     fclose(infile);
